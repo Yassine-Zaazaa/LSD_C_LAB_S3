abdd9c28ed01ce3648e4406011d759d488a96f4d
Yassine-Zaazaa <yassine.zaazaa@um6p.ma>
2021-11-07 15:23:58 +0100

    EX6

--- TP02/EX6/main.c
+++ TP02/EX6/main.c
@@ -1,10 +1,12 @@
 #include "header.h"
 
 int main(){
+    FILE *fp = NULL;
+    fp= fopen("data.txt", "w");
     float *array = (float*)malloc(16 * sizeof(float));
     for(int i = 1; i < 16; i++){
         array[i] = dichotomie(f, 1, 2, i);
-        printf("%lf\t", array[i]);
+        fprintf(fp, "%lf\t %d\n", array[i], i);
     }
     printf("\n");
     free(array);
